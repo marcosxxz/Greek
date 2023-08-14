29334aea537bee3df3d88c5ecd7cd63ca2fb2c7a
marcosxxz <109250880+marcosxxz@users.noreply.github.com>
2023-08-14 18:00:53 -0300

    Add files via upload

---
+++ greek.js
@@ -0,0 +1,16 @@
+// script.js
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.querySelectorAll(".slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 3000); // Altera o slide a cada 3 segundos (3000 milissegundos)
+}
